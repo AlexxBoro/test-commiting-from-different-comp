128404db2aaa5234c40b98fd9d0465f2ba73a0a9
Aleksandra Borowska <aleks.borowska91@gmail.com>
2019-06-05 16:52:08 +0200

    ngFor

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,35 +1,46 @@
-import { Component } from '@angular/core';
-
+import { Component } from "@angular/core";
 
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
   styleUrls: ['./server.component.scss']
 })
-
 export class ServerComponent {
- allowNewServer = false;
- serverCreation = 'no server was created';
- nazwa = 'jakas nazwa';
- serverName = '';
- username = '';
- clickableButtonEnabled = false;
+  allowNewServer = false;
+  serverCreation = 'no server was created';
+  nazwa = 'jakas nazwa';
+  serverName = '';
+  username = '';
+  clickableButtonEnabled = false;
+  serverCreated = false;
+  serverStatus = 'offline';
+  serverId = 10;
+  servers = ['testserver', 'testserver2'];
 
- constructor() {
-  setTimeout(() => {
-    this.allowNewServer = true;
-  }, 2000);
- }
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
 
- onCreateServer() {
-    this.serverCreation = 'server WAS created and the name is:' + this.serverName;
- }
+    this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+  }
 
- onUpdateServerName(event: any) {
-  this.serverName = (<HTMLInputElement>event.target).value;
+  onCreateServer() {
+    this.serverCreation =
+      'server WAS created and the name is:' + this.serverName;
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
+  }
+
+  onUpdateServerName(event: any) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
+
+  getServerStatus() {
+    return this.serverStatus;
+  }
+
+  getColor(){
+    return this.serverStatus === 'online' ? 'yellow' : 'darksalmon';
+  }
 }
-
-}
-
-
-
